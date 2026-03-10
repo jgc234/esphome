0bd8e3456f3efe94263eed74633be3c885b72ff2
Jim Crumpler <jim@crumpler.id.au>
2026-03-10 11:47:00 +1100

    update

--- esphome/components/lorawan/lorawan.h
+++ esphome/components/lorawan/lorawan.h
@@ -12,17 +12,17 @@ namespace lorawan {
 
 class LoRaWANComponent : public Component, public LoraWanSpiRadioLibHal {
  public:
-  void set_chipset(std::string chipset) { chipset_ = chipset; }
-  void set_cs_pin(GPIOPin *pin) { cs_pin_ = pin; }
-  void set_reset_pin(GPIOPin *pin) { reset_pin_ = pin; }
-  void set_busy_pin(GPIOPin *pin) { busy_pin_ = pin; }
-  void set_dio1_pin(GPIOPin *pin) { dio1_pin_ = pin; }
-  void set_band(int band) { band_ = band; }
-  void set_sub_band(uint8_t sub_band) { sub_band_ = sub_band; }
-  void set_join_eui(uint64_t join_eui) { join_eui_ = join_eui; }
-  void set_dev_eui(uint64_t dev_eui) { dev_eui_ = dev_eui; }
-  void set_app_key(uint8_t *app_key) { app_key_ = app_key; }
-  void set_nwk_key(uint8_t *nwk_key) { nwk_key_ = nwk_key; }
+  void set_chipset(std::string chipset) { this->chipset_ = chipset; }
+  void set_cs_pin(GPIOPin *pin) { this->cs_pin_ = pin; }
+  void set_reset_pin(GPIOPin *pin) { this->reset_pin_ = pin; }
+  void set_busy_pin(GPIOPin *pin) { this->busy_pin_ = pin; }
+  void set_dio1_pin(GPIOPin *pin) { this->dio1_pin_ = pin; }
+  void set_band(int band) { this->band_ = band; }
+  void set_sub_band(uint8_t sub_band) { this->sub_band_ = sub_band; }
+  void set_join_eui(uint64_t join_eui) { this->join_eui_ = join_eui; }
+  void set_dev_eui(uint64_t dev_eui) { this->dev_eui_ = dev_eui; }
+  void set_app_key(uint8_t *app_key) { this->app_key_ = app_key; }
+  void set_nwk_key(uint8_t *nwk_key) { this->nwk_key_ = nwk_key; }
 
   void setup() override;
   void loop() override;
@@ -39,13 +39,10 @@ class LoRaWANComponent : public Component, public LoraWanSpiRadioLibHal {
   uint64_t dev_eui_;
   uint8_t *app_key_;
   uint8_t *nwk_key_;
-
   SX1262 *radio_ = nullptr;
   LoRaWANNode *node_ = nullptr;
-
   bool joined_ = false;
   unsigned long last_send_ = 0;
-
   void join_network_();
   void send_uplink_();
 };
